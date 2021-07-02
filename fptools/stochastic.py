8610e34ba7aa92cded70b56847504b4e190fbef7
Garrett Smith <gasmith@uni-potsdam.de>
2021-07-02 14:53:29 +0200

    Undo tqdm thing. Still not working

--- fptools/stochastic.py
+++ fptools/stochastic.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from tqdm import tqdm as tqdm
+from tqdm import tqdm
 
 def ssa_until_abs(W, initidx=0):
     """Run the stochastic simulation algorithm (Gillespie, 1977) until reaching
